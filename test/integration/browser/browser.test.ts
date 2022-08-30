19e7ccbb45baa58c098c8e16b3dbd8db20ed1ff1
Birk Skyum <birk.skyum@pm.me>
2022-08-30 11:01:59 +0200

    browser test keep CJK

--- test/integration/browser/browser.test.ts
+++ test/integration/browser/browser.test.ts
@@ -58,45 +58,6 @@ describe('browser tests', () => {
 
     [chromium].forEach((impl) => {
 
-        test(`${impl.name()} - Drag to the left`, async () => {
-
-            await newTest(impl);
-
-            const canvas = await page.$('.maplibregl-canvas');
-            const canvasBB = await canvas.boundingBox();
-
-            // Perform drag action, wait a bit the end to avoid the momentum mode.
-            await page.mouse.move(canvasBB.x, canvasBB.y);
-            await page.mouse.down();
-            await page.mouse.move(100, 0);
-            await new Promise(r => setTimeout(r, 200));
-            await page.mouse.up();
-
-            const center = await page.evaluate(() => {
-                return map.getCenter();
-            });
-
-            expect(center.lng).toBeCloseTo(-35.15625, 4);
-            expect(center.lat).toBeCloseTo(0, 7);
-        }, 20000);
-
-        test(`${impl.name()} Zoom: Double click at the center`, async () => {
-
-            await newTest(impl);
-            const canvas = await page.$('.maplibregl-canvas');
-            const canvasBB = await canvas.boundingBox();
-            await page.mouse.dblclick(canvasBB.x, canvasBB.y);
-
-            // Wait until the map has settled, then report the zoom level back.
-            const zoom = await page.evaluate(() => {
-                return new Promise((resolve, _reject) => {
-                    map.once('idle', () => resolve(map.getZoom()));
-                });
-            });
-
-            expect(zoom).toBe(2);
-        }, 20000);
-
         test(`${impl.name()} - CJK Characters`, async () => {
             await newTest(impl);
             await page.evaluate(() => {
